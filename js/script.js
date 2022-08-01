446a69cb2f36e48fd3cd52c0366626f1fee92a3c
1huali <wanhuali2000@gmail.com>
2022-08-01 14:06:32 -0400

    Pattern 1 and 2 updated

--- js/script.js
+++ js/script.js
@@ -154,31 +154,33 @@ window.onload = function() {
                 ];
             } else if (currentPatern === PATTERN_2) {
                 let start2X = window.innerWidth / 4;
-                let start2Y = topY + stringUnit * 5;
+                let start2Y = topY + stringUnit * 4;
                 let offset2X = window.innerWidth / 10;
                 xPositions = [
                     start2X,
-                    start2X + offset2X,
-                    start2X + offset2X,
+                    start2X + offset2X+10,
+                    start2X + offset2X+10,
                     start2X + (2 * offset2X + 20),
                     start2X + (2 * offset2X + 20),
                     start2X + (3 * offset2X + 30),
                     start2X + (3 * offset2X + 30),
                     start2X + (4 * offset2X + 40),
                     start2X + (4 * offset2X + 40),
-                    start2X + (2.5 * offset2X + 25)
+                    start2X + (3 * offset2X + 25)
+                    
                 ];
                 yPositions = [
-                    start2Y,
-                    start2Y + stringUnit,
-                    start2Y - stringUnit,
-                    start2Y + (1.5 * stringUnit + 15),
-                    start2Y - (1.5 * stringUnit + 15),
-                    start2Y + (2 * stringUnit + 20),
-                    start2Y - (2 * stringUnit + 20),
-                    start2Y + (2.5 * stringUnit + 25),
-                    start2Y - (2.5 * stringUnit + 25),
-                    start2Y + (40 + stringUnit)
+                    (start2Y)-35,
+                    (start2Y + stringUnit+5)-35,
+                    (start2Y - stringUnit+5)-35,
+                   ( start2Y + (1.5 * stringUnit + 10))-35,
+                   ( start2Y - (1.5 * stringUnit + 10))-35,
+                    (start2Y + (2 * stringUnit + 15))-35,
+                    (start2Y - (2 * stringUnit + 15))-35,
+                   ( start2Y + (2.5 * stringUnit + 20))-35,
+                  (  start2Y - (2.5 * stringUnit + 20)-35),
+                    start2Y + (3 * stringUnit + 100)
+
                 ];
             } else if (currentPatern === PATTERN_3) {
                 let start3X = window.innerWidth / 2;
@@ -193,16 +195,25 @@ window.onload = function() {
                     start3X - 300 / 2,
                     start3X + 300 / 2,
                     start3X
+                    
+                    // start3X,
+                    // start3X - 100 / 2,
+                    // start3X + 100 / 2,
+                    // start3X - 200 / 2,
+                    // start3X + 200 / 2,
+                    // start3X - 300 / 2,
+                    // start3X + 300 / 2,
+                    // start3X
                 ];
                 yPositions = [
-                    start3Y + 300 / 2,
+                    start3Y + (300 / 2)-50,
                     start3Y,
                     start3Y,
-                    start3Y - 100 / 2,
-                    start3Y - 100 / 2,
+                    start3Y - (100 / 2)-50,
+                    start3Y - (100 / 2)-50,
                     start3Y,
                     start3Y,
-                    start3Y - 200 / 2
+                    start3Y - (200 / 2)-50
                 ];
             }
 
@@ -222,20 +233,22 @@ window.onload = function() {
         let chime6 = new Chimes(stringchime6, document.getElementById(`chime6`), chimePositions.x[6], chimePositions.y[6], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit * 3);
 
         chimePositions = defineChimesPosition(PATTERN_2);
-
-        let chime8 = new Chimes(stringchime8, document.getElementById(`chime8`), chimePositions.x[1], chimePositions.y[1], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
+        //modif aug1 : chimePosition array had mistyping - 11 unités
         let chime7 = new Chimes(stringchime7, document.getElementById(`chime7`), chimePositions.x[0], chimePositions.y[0], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 0, stringUnit);
+        let chime8 = new Chimes(stringchime8, document.getElementById(`chime8`), chimePositions.x[1], chimePositions.y[1], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
         let chime9 = new Chimes(stringchime9, document.getElementById(`chime9`), chimePositions.x[2], chimePositions.y[2], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
-        let chime10 = new Chimes(stringchime10, document.getElementById(`chime10`), chimePositions.x[2], chimePositions.y[2], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
-        let chime11 = new Chimes(stringchime11, document.getElementById(`chime11`), chimePositions.x[3], chimePositions.y[2], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
-        let chime12 = new Chimes(stringchime12, document.getElementById(`chime12`), chimePositions.x[4], chimePositions.y[4], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
-        let chime13 = new Chimes(stringchime13, document.getElementById(`chime13`), chimePositions.x[5], chimePositions.y[5], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
-        let chime14 = new Chimes(stringchime14, document.getElementById(`chime14`), chimePositions.x[6], chimePositions.y[6], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
-        let chime15 = new Chimes(stringchime15, document.getElementById(`chime15`), chimePositions.x[7], chimePositions.y[7], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
-        let chime16 = new Chimes(stringchime16, document.getElementById(`chime16`), chimePositions.x[8], chimePositions.y[8], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
+        let chime10 = new Chimes(stringchime10, document.getElementById(`chime10`), chimePositions.x[3], chimePositions.y[3], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
+        let chime11 = new Chimes(stringchime11, document.getElementById(`chime11`), chimePositions.x[4], chimePositions.y[4], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
+        let chime12 = new Chimes(stringchime12, document.getElementById(`chime12`), chimePositions.x[5], chimePositions.y[5], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
+        let chime13 = new Chimes(stringchime13, document.getElementById(`chime13`), chimePositions.x[6], chimePositions.y[6], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
+        let chime14 = new Chimes(stringchime14, document.getElementById(`chime14`), chimePositions.x[7], chimePositions.y[7], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
+        let chime15 = new Chimes(stringchime15, document.getElementById(`chime15`), chimePositions.x[8], chimePositions.y[8], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 500, stringUnit);
+        // let chime16 = new Chimes(stringchime16, document.getElementById(`chime16`), chimePositions.x[9], chimePositions.y[9], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit);
+        let chime17 = new Chimes(stringchime17, document.getElementById(`chime17`), chimePositions.x[9], chimePositions.y[9], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit - 70);
 
         chimePositions = defineChimesPosition(PATTERN_3);
-        let chime17 = new Chimes(stringchime17, document.getElementById(`chime17`), chimePositions.x[0], chimePositions.y[0], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit - 70);
+//modif aug1: 17 monté à pattern 2
+
         let chime18 = new Chimes(stringchime18, document.getElementById(`chime18`), chimePositions.x[1], chimePositions.y[1], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit + 70);
         let chime19 = new Chimes(stringchime19, document.getElementById(`chime19`), chimePositions.x[2], chimePositions.y[2], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit + 70);
         let chime20 = new Chimes(stringchime20, document.getElementById(`chime20`), chimePositions.x[3], chimePositions.y[3], document.getElementById(`dustSound`), document.getElementById(`dustSound2`), currentLook, currentChimeForm, 1000, stringUnit + 70);
@@ -290,7 +303,7 @@ window.onload = function() {
             stringChimeArray.push(stringchime13);
             stringChimeArray.push(stringchime14);
             stringChimeArray.push(stringchime15);
-            stringChimeArray.push(stringchime16);
+            // stringChimeArray.push(stringchime16);
             stringChimeArray.push(stringchime17);
 
             chimesArray = [];
@@ -303,7 +316,7 @@ window.onload = function() {
             chimesArray.push(chime13);
             chimesArray.push(chime14);
             chimesArray.push(chime15);
-            chimesArray.push(chime16);
+            // chimesArray.push(chime16);
             chimesArray.push(chime17);
             topPlate.setCorePosition(chime11);
         }
