2a8b2abe6a9f9ed65c8b16b5ec45f071fdfe45d0
1huali <wanhuali2000@gmail.com>
2022-08-01 20:41:01 -0400

    ^

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ window.onload = function() {
         //buttons
         let light = true;
         let toggle = true;
-        // let currentForceModeTextZone = document.getElementById(`currentForceMode`);
+        let currentForceModeTextZone = document.getElementById(`currentForceMode`);
         let forceMode = `Mouse/Tap Input`;
 
         //variable data
